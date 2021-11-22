436103eb1e0f6ca929b3b6da654fa10e5f5542ce
BlackBerryID <iddf1994one@gmail.com>
2021-11-22 17:13:21 +0300

    feat: animate category opening

--- src/js/initial-menu.js
+++ src/js/initial-menu.js
@@ -23,23 +23,57 @@ export default class Menu {
   }
 
   openCategory(e) {
+    const targetCategory = e.path[1].classList.contains("artists")
+      ? "artists"
+      : "pictures";
+    this.animateOpening(targetCategory);
     this.initialMenu.style.setProperty("pointer-events", "none");
-    this.initialMenu.style.setProperty("opacity", "0");
-    setTimeout(showCategoryPage.bind(this), 200);
-    function showCategoryPage() {
-      this.initialMenu.style.setProperty("display", "none");
-      this.initialMenu.style.setProperty("pointer-events", "initial");
-      this.categoryPage.style.setProperty("display", "block");
-      setTimeout(() => this.categoryPage.style.setProperty("opacity", "1"), 50);
+    setTimeout(() => {
+      this.initialMenu.style.setProperty("opacity", "0");
+      setTimeout(showCategoryPage.bind(this), 200);
+      function showCategoryPage() {
+        this.initialMenu.style.setProperty("display", "none");
+        this.initialMenu.style.setProperty("pointer-events", "initial");
+        this.categoryPage.style.setProperty("display", "block");
+        this.restoreMenuAppearance(targetCategory);
+        setTimeout(
+          () => this.categoryPage.style.setProperty("opacity", "1"),
+          50
+        );
+      }
+      this.player.volume = settingsData.volume;
+      this.player.play();
+      if (targetCategory === "artists") {
+        settingsData.activeCategory = "artists";
+        this.category.show("artists");
+      } else if (targetCategory === "pictures") {
+        settingsData.activeCategory = "pictures";
+        this.category.show("pictures");
+      }
+    }, 1000);
+  }
+
+  animateOpening(category) {
+    if (category === "artists") {
+      this.artistsGameBtn.style.setProperty("transform", "translateY(50%)");
+      this.picturesGameBtn.style.setProperty("transform", "scale(0.1)");
+      this.picturesGameBtn.style.setProperty("opacity", "0");
+    } else if (category === "pictures") {
+      this.picturesGameBtn.style.setProperty("transform", "translateY(-50%)");
+      this.artistsGameBtn.style.setProperty("transform", "scale(0.1)");
+      this.artistsGameBtn.style.setProperty("opacity", "0");
     }
-    this.player.volume = settingsData.volume;
-    this.player.play();
-    if (e.path[1].classList.contains("artists")) {
-      settingsData.activeCategory = "artists";
-      this.category.show("artists");
-    } else if (e.path[1].classList.contains("pictures")) {
-      settingsData.activeCategory = "pictures";
-      this.category.show("pictures");
+  }
+
+  restoreMenuAppearance(category) {
+    if (category === "artists") {
+      this.artistsGameBtn.style.setProperty("transform", "translateY(0)");
+      this.picturesGameBtn.style.setProperty("transform", "scale(1)");
+      this.picturesGameBtn.style.setProperty("opacity", "1");
+    } else if (category === "pictures") {
+      this.picturesGameBtn.style.setProperty("transform", "translateY(0)");
+      this.artistsGameBtn.style.setProperty("transform", "scale(1)");
+      this.artistsGameBtn.style.setProperty("opacity", "1");
     }
   }
 
